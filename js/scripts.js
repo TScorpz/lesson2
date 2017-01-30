ff642f21cdef516121caa01fc3b47741cff00486
TScorpz <shamaner88@gmail.com>
2017-01-31 01:20:39 +0200

    Html css changes
    Added bootstrap.
    Added registration.html page.
    Renamed style.css to styles.css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-    $("#signInBtn").on("click", function(){
+    $('#signInBtn').on('click', function(){
         alert("Wow");
+    });
+    $('#regBtn').on('click', function(){
+        window.location.href = 'registration.html';
     });
     $(".box").on('click', function(){
       var animations = ['crawl', 'dangle', 'jump', 'stretch', 'flip'];
@@ -12,5 +15,25 @@ $(document).ready(function(){
       setTimeout(function() {
             $(box).removeClass(animation);
       }, 4000);
+    });
+    $('#registration-submit').on('click', function() {
+      var email = $('#email').val();
+
+      var formFilled = true;
+      if ($('#username').val() == '') {
+        formFilled = false;
+        $('#username-warning').show();
+      }
+      if ($('#password').val() == '') {
+        formFilled = false;
+        $('#password-warning').show();
+      }
+      if ($('#email').val() == '') {
+        formFilled = false;
+        $('#email-warning').show();
+      }
+      if (formFilled) {
+        $('.success').show();
+      }
     });
 });
